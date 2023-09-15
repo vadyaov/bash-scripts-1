c4fba6a772a1799d9651fa85e7353b3bbe022b48
vadim <vamyakish@mail.ru>
2023-09-15 14:30:55 +0300

    3 done

--- src/03/main.sh
+++ src/03/main.sh
@@ -19,6 +19,17 @@ else
   if [[ $1 =~ $reg && $2 =~ $reg && $3 =~ $reg && $4 =~ $reg ]]; then
     if [[ $1 -eq $2 || $3 -eq $4 ]]; then
       echo "Background color and text color must be different."
+      read -p "Do you want to run the script again with new arguments? (Y/N): " answer
+        case "$answer" in
+          [Yy]*)
+            read -p "Enter 4 new arguments separated by spaces: " new_args
+            set -- $new_args
+            exec "$0" "$@"
+            ;;
+          *)
+            echo "Exiting script."
+            ;;
+        esac
     else
       source data.sh
       source colors.sh
